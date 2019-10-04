58ed386c6d37687b35c937a10b1c62686223d709
kuzmich84 <kudim@inbox.ru>
2019-10-04 16:25:49 +0300

    create module util.js

---
+++ js/util.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function () {
+
+  window.util = {
+    getDeclensionWord: function (number, word) {
+      var cases = [2, 0, 1, 1, 1, 2];
+      return number + word[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+    },
+
+    setAttributeDisabled: function (formFields) {
+      for (var i = 0; i < formFields.length; i++) {
+        formFields[i].setAttribute('disabled', 'disabled');
+      }
+    },
+
+    setAttributeEnabled: function (formFields) {
+      for (var i = 0; i < formFields.length; i++) {
+        formFields[i].removeAttribute('disabled');
+      }
+    },
+  };
+
+})();
