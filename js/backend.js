ecba8175396323e8ee7b8a953c70c66abc610f0d
Dmitry Kuchukov <kudim84@yandex.ru>
2019-11-03 13:53:53 +0300

    add module backend.js

---
+++ js/backend.js
@@ -0,0 +1,41 @@
+'use strict';
+
+(function () {
+  var URL_LOAD = 'https://js.dump.academy/keksobooking/data';
+  var URL_UPLOAD = 'https://js.dump.academy/keksobooking';
+
+  var connectXhr = function (method, url, onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open(method, url);
+    return xhr;
+  };
+
+  window.backend = {
+    load: function (onSuccess, onError) {
+      connectXhr('GET', URL_LOAD, onSuccess, onError).send();
+    },
+    upload: function (data, onSuccess, onError) {
+      connectXhr('POST', URL_UPLOAD, onSuccess, onError).send(data);
+    }
+  };
+
+})();
